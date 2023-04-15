91e2ceb552e089d4b762c4e44e39d2becbcc9e66
Jhonatan de Oliveira <jhonatandevbr@gmail.com>
2023-04-15 15:00:29 +0900

    .
    .

---
+++ INTRODUCAO EM C/caixa eletronico/caixaEletro.cpp
@@ -0,0 +1,21 @@
+/* 
+um caixa eletronico possui notas de 50, 20, 10
+faca um programa que calcule quantas notas de cada deve 
+ser fornecida para o cliente ao realizar um saque 
+de um determinado valor
+*/
+
+#include <stdio.h>
+#include <iostream>
+
+using namespace std; //habilitando cout e cin 
+
+int main(void){
+  double value;
+  cout << "Digite o valor a ser sacado.";
+  cin >> value;
+  double result;
+  
+  
+
+}
